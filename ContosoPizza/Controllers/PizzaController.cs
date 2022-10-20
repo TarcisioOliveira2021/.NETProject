a24c8a9b1aee8a3d96969022c38ec1000f0d463c
tarcisio.veloso <tarcisio.veloso@tcm.ba.gov.br>
2022-10-20 17:13:46 -0300

    Subindo a finalização do Costoco project.

--- ContosoPizza/Controllers/PizzaController.cs
+++ ContosoPizza/Controllers/PizzaController.cs
@@ -32,8 +32,48 @@ public class PizzaController : ControllerBase
 
 
     // POST action
+    [HttpPost]
+    public IActionResult Create(Pizza pizza)
+    {
+        PizzaService.Add(pizza);
+        return CreatedAtAction(nameof(Create), new {id = pizza.Id}, pizza);
+    }
 
     // PUT action
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, Pizza pizza)
+    {
+        //Caso o ID seja diferente to tipo id temos um err 400.
+        if(id != pizza.Id){
+            return BadRequest();
+        }
+
+        // Caso a pizza não seja encontrada.
+        var isExistPizza = PizzaService.Get(id);
+        if(isExistPizza is null){
+            return NotFound();
+        }
+
+        //Dando update na pizza
+        PizzaService.Update(pizza);
+        return NoContent();
+    }
 
     // DELETE action
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        //Armazena a pizza para deletar.
+        var pizzErase = PizzaService.Get(id);
+
+        //Verifica se a pizza existe.
+        if(pizzErase is null){
+            return NotFound();
+        }
+
+        //Deleta a pizza.
+        PizzaService.Delete(id);
+        return NoContent();
+    }
+
 }
