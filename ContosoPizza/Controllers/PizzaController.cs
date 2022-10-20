d25936abcf32ab68d84c7c3f408351542b2b8103
tarcisio.veloso <tarcisio.veloso@tcm.ba.gov.br>
2022-10-20 16:37:54 -0300

    Projeto 1 - pizza - Microsoft

---
+++ ContosoPizza/Controllers/PizzaController.cs
@@ -0,0 +1,39 @@
+using ContosoPizza.Models;
+using ContosoPizza.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoPizza.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PizzaController : ControllerBase
+{
+    public PizzaController()
+    {
+    }
+
+    // GET all action
+    [HttpGet]
+    public ActionResult<List<Pizza>> listAll() => PizzaService.GetAll();
+
+    // GET by Id action
+    [HttpGet("{id}")]
+    public ActionResult<Pizza> FindById(int id)
+    {
+        var Pizza = PizzaService.Get(id);
+
+        if(Pizza == null)
+        {
+            return NotFound();
+        }
+
+        return Pizza;
+    }
+
+
+    // POST action
+
+    // PUT action
+
+    // DELETE action
+}
