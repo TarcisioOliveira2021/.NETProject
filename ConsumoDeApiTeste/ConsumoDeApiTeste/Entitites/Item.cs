80f6221bdf0353f70b092f112fdfd5e686bac897
tarcisio.veloso <tarcisio.veloso@tcm.ba.gov.br>
2022-11-08 16:57:23 -0300

    Projeto de consumo de API.NET (INCOMPLETO)

---
+++ ConsumoDeApiTeste/ConsumoDeApiTeste/Entitites/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsumoDeApiTeste.Entitites
+{
+    public class Item
+    {
+        public string esfera { get; set; }
+        public string demonstrativo { get; set; }
+        public string anexo { get; set; }
+    }
+}
---
+++ ConsumoDeApiTeste/ConsumoDeApiTeste/Program.cs
@@ -0,0 +1,58 @@
+﻿using ConsumoDeApiTeste.Entitites;
+using Newtonsoft.Json;
+
+namespace ConsumoDeApiTeste { 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Listar();
+        }
+
+        public static void Listar()
+        {
+            HttpClient client = new HttpClient();
+            var response = client.GetStringAsync("https://apidatalake.tesouro.gov.br/ords/siconfi/tt/anexos-relatorios");
+            response.Wait();
+            var resp = JsonConvert.DeserializeObject<Anexo[]>(response.Result).ToList();
+            
+
+        }
+
+        /*
+         * 
+         * public static void Listar()
+            {
+             string urlApi = "https://jsonplaceholder.typicode.com/users";
+            try
+            {
+                using(var client = new HttpClient())
+                {
+                    var response = client.GetStringAsync(urlApi);
+                    response.Wait();
+
+                    var resp = JsonConvert.DeserializeObject<User[]>(response.Result).ToList();
+
+                    foreach(var user in resp)
+                    {
+                        Console.WriteLine(user.id);
+                        Console.WriteLine(user.name);
+                        Console.WriteLine(user.username);
+                        Console.WriteLine(user.email);
+                        Console.WriteLine(user.address);
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                throw;
+            }
+            }
+         * 
+         * 
+         * https://www.newtonsoft.com/json/help/html/DeserializeCollection.htm
+         */
+
+    }
+}
+
