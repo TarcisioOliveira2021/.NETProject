f62c4bd688f624711f4547fbbb42dca6907f2658
tarcisio.veloso <tarcisio.veloso@tcm.ba.gov.br>
2023-03-14 16:45:54 -0300

    Subindo projeto consumo de CSV.

---
+++ POC_CsvReader/FormaAlternativa/Estados.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace POC_CsvReader.FormaAlternativa
+{
+    public class Estados
+    {
+        public int COD { get; set; }
+        public string NOME { get; set; }
+        public string SIGLA { get; set; }
+    }
+}
---
+++ POC_CsvReader/Program.cs
@@ -0,0 +1,44 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using POC_CsvReader.FormaAlternativa;
+using System.Globalization;
+
+namespace POC_Steam_StreamWritter{
+    class Program {
+        private const string pathCsv = @"C:\PATH\FILENAME.csv";
+        private const string pathText = @"C:\PATH\FILENAME.txt";
+
+
+        public static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+            class1.Start();
+        }
+
+        //static void Main(string[] args)
+        //{
+        //    //Cria configurações no Culture.
+        //    var confCulture = new CsvConfiguration(CultureInfo.CurrentCulture)
+        //    {
+        //        Delimiter = ","
+        //    };
+
+        //    int cont = 1;
+
+        //    var leitorStream = new StreamReader(pathText);
+        //    var arquivoCsv = new CsvReader(leitorStream, confCulture);
+            
+        //    while (arquivoCsv.Read())
+        //    {
+        //        Console.WriteLine($"=========={cont}==========");
+        //        Console.WriteLine(arquivoCsv.Parser.RawRecord);
+        //        Console.WriteLine($"====================");
+        //        cont++;
+        //    }
+            
+        //}
+
+        
+    }
+}
+
