920f56ed230af564cbbd611b5e849ddd908154ca
tarcisio <tarcisio.veloso@ucsal.edu.br>
2023-03-17 21:21:45 -0300

    Subindo arquivo.

---
+++ MeuTreinadorMatematicaBasica/MeuTreinadorMatematicaBasica/Model/ViewModelSoma.cs
@@ -0,0 +1,10 @@
+﻿namespace MeuTreinadorMatematicaBasica.Model
+{
+    public class ViewModelSoma
+    {
+        public int Valor1 { get; set; }
+        public int Valor2 { get; set; }
+        public int Resposta { get; set; }
+        public string CorretoIncorreto { get; set; }
+    }
+}
